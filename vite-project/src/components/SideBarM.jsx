3e69aa25a12ba076192a6e08e68816c9c54a83ed
dilucas00 <165342425+dilucas00@users.noreply.github.com>
2025-02-18 04:13:07 +0000

    Inicializar o projeto e SideBar feita

---
+++ vite-project/src/components/SideBarM.jsx
@@ -0,0 +1,23 @@
+
+import '../App.css'
+
+function SideBarM() {
+
+
+  return (
+    <>
+     <div className='w-m h-15 bg-[#112942] flex flex-row justify-center  '>
+        <div className='flex justify-center items-center'>
+          <div className='flex items-center justify-center w-10 '>
+            <img src="/Menu.png" alt="Dropdown Menu" className='w-8 h-8 mr-20 ' />
+          </div>
+          <div className=''>
+            <img src="/finxi-logo.png" alt="" className='w-60' /> 
+          </div>
+        </div>  
+     </div>
+    </>
+  )
+}
+
+export default SideBarM
---
+++ vite-project/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import SideBarM from './components/SideBarM';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  <SideBarM></SideBarM>
+  
+  </StrictMode>,
+)
