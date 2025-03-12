5543432129ce878fb24c8d7a5af9304eb45b1dd2
dilucas00 <165342425+dilucas00@users.noreply.github.com>
2025-03-12 12:39:16 +0000

    mudanças na pagina login

--- finxi-webapp/src/components/Login.jsx
+++ finxi-webapp/src/components/Login.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import "../Login.css"
 
 function Login(){
     return(
-        <div> <h1>VOCE ESTA NA PAGINA DE LOGIN!!!</h1></div>
+       <div className='w-full h-full'>
+        <div className='flex flex-col justify-center items-center content-center   '>
+            <img src="/public/finxi-logo.png" className='w-50 h-50' alt="" />
+        </div>
+        <div>
+            <div>
+                <span className='text-white'>Crie sua conta</span>
+            </div>
+            <div>
+    
+                
+             
+            </div>
+        </div>
+
+       </div>
+        
     )
 }
 
