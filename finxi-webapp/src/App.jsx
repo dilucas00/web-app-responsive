d7032c001548b8da7c8beb6d890b3632630ce8d5
dilucas00 <165342425+dilucas00@users.noreply.github.com>
2025-03-12 00:56:48 +0000

    sistema de rotas criado

--- finxi-webapp/src/App.jsx
+++ finxi-webapp/src/App.jsx
@@ -1,16 +1,24 @@
 import "./App.css"
 import React from 'react';
 import Home from "./components/Home"
+import Login from "./components/Login"
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import 'antd/dist/reset.css'; 
 
 
-function App() {
-
-
+const App = () => {
   return (
-    <Home></Home>
-   
-  )
-}
+    <Router>
+      <nav>
+        <Link to="/"></Link>
+        <Link to="/login"></Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
