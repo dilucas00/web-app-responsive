afd8e1de4cd36bb520f173e39b34e11f5fef061c
dilucas00 <165342425+dilucas00@users.noreply.github.com>
2025-03-12 17:21:48 +0000

    estilizações do login

--- finxi-webapp/src/components/Login.jsx
+++ finxi-webapp/src/components/Login.jsx
@@ -8,14 +8,26 @@ function Login(){
             <img src="/public/finxi-logo.png" className='w-50 h-50' alt="" />
         </div>
         <div>
-            <div>
-                <span className='text-white'>Crie sua conta</span>
+            <div className='flex flex-col items-center gap-10'>
+                <div className='flex items-start w-60'>
+                    <h4 className='text-white'>Crie sua conta</h4>
+                </div>
+                <input className='bg-white w-60 h-8 '></input>
+                <input className='bg-white w-60 '></input>
+                <input className='bg-white w-60 '></input>
+                <button className='bg- text-white'>Buscar</button>
             </div>
             <div>
-    
-                
-             
+                <p>Ou cadastre-se com:</p>
             </div>
+            <div>
+                Icone Facebook
+            </div>
+            <div>
+                Icone Google
+            </div>
+           
+           
         </div>
 
        </div>
