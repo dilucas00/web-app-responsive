c1f4ec5a02ba6824f7d685e739948100bea6025a
dilucas00 <165342425+dilucas00@users.noreply.github.com>
2025-03-14 02:23:43 +0000

    criaçao do componente de login

--- finxi-webapp/src/App.jsx
+++ finxi-webapp/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import React from 'react';
 import Home from "./components/Home"
+import Login from "./components/Login"
 import Register from "./components/Register"
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import 'antd/dist/reset.css'; 
@@ -12,10 +13,12 @@ const App = () => {
       <nav>
         <Link to="/"></Link>
         <Link to="/register"></Link>
+        <Link to="/login" ></Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </Router>
   );
--- finxi-webapp/src/components/Login.jsx
+++ finxi-webapp/src/components/Login.jsx
@@ -2,26 +2,22 @@ import React from 'react'
 import "../Login.css"
 import { useNavigate } from 'react-router-dom';
 
-function Register(){
+function Login() {
     const navigate = useNavigate();
-
+    
     const handleHomeClick = () => {
         navigate('/');
-      };
-
-    const handleLoginClick = () =>{
-        navigate('/login');
     };
 
     return(
-       <div className='w-full bg-[#112942] h-auto'>
+        <div className='w-full bg-[#112942] h-auto'>
         <div className='flex flex-col justify-center items-center content-center    '>
             <img src="/icons/finxi-logo.png" className='w-60 h-60' alt="" />
         </div>
         <div>
             <div className='flex flex-col items-center gap-8'>
                 <div className='flex items-center justify-center w-full'>
-                    <h4 className='text-white text-2xl font-oswald'>Crie sua conta</h4>
+                    <h4 className='text-white text-2xl font-oswald'>Faça o Login</h4>
                 </div>
 
                 {/* INPUT EMAIL */}
@@ -73,38 +69,13 @@ function Register(){
                 />
                 </div>
 
-                {/* INPUT CONFIRMAR SENHA */}
-
-                <div
-                className="shadow-lg flex gap-2 items-center bg-white p-2 hover:shadow-xl  duration-300 hover:border-2 border-gray-400 group rounded-md"
-                >
-                <svg
-                    class="group-hover:rotate-[360deg] duration-300"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    height="1.5em"
-                    width="1.5em"
-                >
-                   <path d="M6 10V7a6 6 0 0 1 12 0v3" />
-                    <rect x="4" y="10" width="16" height="12" rx="2" />
-                    <path d="M12 16v2" />
-
-                </svg>
-                <input
-                    type="password"
-                    name="password"
-                    className="flex-1  focus:outline-none"
-                    placeholder="Confirmar senha"
-                />
-                </div>
 
                 <div className='m-5'>
-                    <button onClick={handleHomeClick} className='bg-[#7BDAA5] text-white w-65 h-10 text-sm font-bold shadow-black shadow font-montserrat cursor-pointer hover:scale-105 transition'>Cadastrar</button>
+                    <button onClick={handleHomeClick} className='bg-[#7BDAA5] text-white w-65 h-10 text-sm font-bold shadow-black shadow font-montserrat cursor-pointer hover:scale-105 transition'>Entrar</button>
                 </div>
             </div>
             <div className='flex items-centercenter justify-center pt-5 w-full'>
-                <p className='text-white font-montserrat text-xl '>Ou cadastre-se com:</p>
+                <p className='text-white font-montserrat text-xl '>Ou faça login com:</p>
             </div>
             <div className='flex flex-row gap-5 justify-center items-center h-30'>
                 <div className='m-2 w-25 h-15 bg-transparent flex justify-center items-center shadow shadow-black cursor-pointer hover:scale-105 transition'>
@@ -117,11 +88,12 @@ function Register(){
         </div>
         <footer>
             <div className='flex justify-center items-center border-t-1 border-gray-600 font-montserrat text-xl p-5 text-white'>
-                <p>Ja tem conta? Clique <a onClick={ handleLoginClick} className='font-bold cursor-pointer text-[#7BDAA5]'> aqui </a></p>
+                <p>Esqueceu sua senha? Clique <a className='font-bold cursor-pointer text-[#7BDAA5] '> aqui </a></p>
             </div>
         </footer>
        </div>
+
     )
 }
 
-export default Register;
+export default Login;
